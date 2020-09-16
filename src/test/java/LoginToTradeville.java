9a76ac4c3443dc2e46970890ce4938c883b6f27e
Radu Profeanu <profeanu.radu@yahoo.com>
2020-09-16 14:41:10 +0300

    Adding modification to login to Tradeville

--- src/test/java/LoginToTradeville.java
+++ src/test/java/LoginToTradeville.java
@@ -3,8 +3,14 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.util.Base64;
+
 public class LoginToTradeville {
     public static void main(String[] args) {
+
+        byte[] decodedBytes= Base64.getDecoder().decode("SW5uaXN0cmFkMQ==");
+        String decodeString= new String(decodedBytes);
+
         System.setProperty("webdriver.chrome.driver","/Chromedriver/chromedriver.exe");
 
         WebDriver driver=new ChromeDriver();
@@ -18,7 +24,7 @@ public class LoginToTradeville {
 
         WebElement password =driver.findElement(By.xpath("//*[@id=\"ctl00_phContent_ucComposeLogin_ucLoginStartrade_pnlLoginStartrade\"]/input[2]"));
         password.click();
-        password.sendKeys("Innistrad1");
+        password.sendKeys(String.valueOf(decodeString));
 
         driver.findElement(By.xpath("//*[@id=\"ctl00_phContent_ucComposeLogin_ucLoginStartrade_btnLogin\"]")).click();
     }
