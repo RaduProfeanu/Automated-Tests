d103e885eec5ae03a68ca317e94acecd54eeec79
Radu Profeanu <profeanu.radu@yahoo.com>
2020-09-16 18:53:29 +0300

    Adding modification to login to Tradeville

--- src/test/java/LoginToTradeville.java
+++ src/test/java/LoginToTradeville.java
@@ -3,17 +3,30 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-import java.util.Base64;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 public class LoginToTradeville {
     public static void main(String[] args) {
 
-        byte[] decodedBytes= Base64.getDecoder().decode("SW5uaXN0cmFkMQ==");
-        String decodeString= new String(decodedBytes);
+        String data = null;
 
-        System.setProperty("webdriver.chrome.driver","/Chromedriver/chromedriver.exe");
+        try {
+           File myObj = new File("C:\\Users\\Radu\\Desktop\\java\\Tradeville.txt");
+            Scanner myReader = new Scanner(myObj);
+               while (myReader.hasNextLine()) {
+                data = myReader.nextLine();
+            }
+            myReader.close();
+            //   data1=data;
+        } catch (FileNotFoundException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+        System.setProperty("webdriver.chrome.driver", "/Chromedriver/chromedriver.exe");
 
-        WebDriver driver=new ChromeDriver();
+        WebDriver driver = new ChromeDriver();
         driver.get("https://tradeville.eu/");
 
         driver.findElement(By.id("ac--two")).click();
@@ -22,9 +35,10 @@ public class LoginToTradeville {
         utilizator.click();
         utilizator.sendKeys("Pradu");
 
-        WebElement password =driver.findElement(By.xpath("//*[@id=\"ctl00_phContent_ucComposeLogin_ucLoginStartrade_pnlLoginStartrade\"]/input[2]"));
+        WebElement password = driver.findElement(By.xpath("//*[@id=\"ctl00_phContent_ucComposeLogin_ucLoginStartrade_pnlLoginStartrade\"]/input[2]"));
         password.click();
-        password.sendKeys(String.valueOf(decodeString));
+        password.sendKeys(String.valueOf(data));
+        //password.sendKeys(String.valueOf(data1));
 
         driver.findElement(By.xpath("//*[@id=\"ctl00_phContent_ucComposeLogin_ucLoginStartrade_btnLogin\"]")).click();
     }
